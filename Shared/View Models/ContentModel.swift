89cb606905d4c4b906c03fd84f53108cf6a82451
Ekkehard <emkinter@me.com>
2022-04-06 08:12:42 -0600

    Reorganize LearningApp

---
+++ Shared/View Models/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  Learning App
+//
+//  Created by Ekkehard Koch on 2022.04.06.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+}
--- Shared/Views/HomeView.swift
+++ Shared/Views/HomeView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
     }
