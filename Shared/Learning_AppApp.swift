e898c807ab7a2702805f2d579426279c4172dd16
Ekkehard <emkinter@me.com>
2022-04-06 07:55:51 -0600

    Initial Commit

---
+++ Shared/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Shared
+//
+//  Created by Ekkehard Koch on 2022.04.06.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
