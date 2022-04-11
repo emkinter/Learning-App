d84f78b6fdf1ae7974326dfa0ddd0d07677051d0
Ekkehard <emkinter@me.com>
2022-04-11 08:32:21 -0600

    • Add HomeView • Add HomeViewRow

--- Shared/Views/HomeView.swift
+++ Shared/Views/HomeView.swift
@@ -12,8 +12,26 @@ struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("What do you want to do today?")
+                    .padding(.leading, 20)
+                ScrollView {
+                    LazyVStack {
+                        VStack(spacing: 20) {
+                            ForEach(model.modules){ module in
+                                // MARK: Learning Card
+                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                // MARK: Test Card
+                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Questions", time: module.test.time)
+                            }
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Get Started")
+        }
     }
 }
 
@@ -21,6 +39,7 @@ struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
